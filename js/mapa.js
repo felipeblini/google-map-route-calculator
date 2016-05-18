193483e40213ff86b50fff057ac86efdfa7856b3
Felipe blini <Felipe.blini@gmail.com>
2016-05-18 18:47:58 -0300

    Created file PontosInteresse.kml, put online and using it as a kmlLayer for the map with the placemarks showing in the map

--- js/mapa.js
+++ js/mapa.js
@@ -10,117 +10,111 @@ window.onload = function () {
 
     });
 
-    //cria e adiciona o ponteiro (marker) "Move me"
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: true,
-        icon: '/img/usu-pointer.png'
-    });
+    var kmlUrl2 = 'http://felipeblini-001-site32.btempurl.com/PontosInteresse.kml';
+    var kmlOptions = {
+        suppressInfoWindows: true,
+        preserveViewport: false,
+        map: map
+    };
+    
+    var kmlLayer = new google.maps.KmlLayer(kmlUrl2, kmlOptions);
 
-    marker.setPosition(latlng);
+    //evento chamado toda vez que o usuario mexe no "Move me"
+    // geocoder = new gm.Geocoder();
 
-    //evento chamado toda vez que o usuário mexe no "Move me"
-    geocoder = new gm.Geocoder();
+    // gm.event.addListener(marker, 'drag', function () {
+    //     geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
+    //         if (status == gm.GeocoderStatus.OK) {
+    //             if (results[0]) {
 
-    gm.event.addListener(marker, 'drag', function () {
-        geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
-            if (status == gm.GeocoderStatus.OK) {
-                if (results[0]) {
+    //                 var latitude = marker.getPosition().lat();
+    //                 var longitude = marker.getPosition().lng()
+    //                 latlng = new gm.LatLng(latitude, longitude);
 
-                    var latitude = marker.getPosition().lat();
-                    var longitude = marker.getPosition().lng()
-                    latlng = new gm.LatLng(latitude, longitude);
+    //                 for (var i = 0; i < markersArray.length; i++) {
+    //                     markersArray[i].setMap(null);
+    //                 }
+    //                 markersArray.length = 0;
 
-                    for (var i = 0; i < markersArray.length; i++) {
-                        markersArray[i].setMap(null);
-                    }
-                    markersArray.length = 0;
+    //                 carregarPontos();
+    //             }
+    //         }
+    //     });
+    // });
 
-                    carregarPontos();
-                }
-            }
-        });
-    });
+    // carregarPontos();
 
-    carregarPontos();
+    // function carregarPontos() {
 
-    function carregarPontos() {
+    //     var iw = new gm.InfoWindow();
+    //     var oms = new OverlappingMarkerSpiderfier(map,
+    //       { markersWontMove: true, markersWontHide: true });
 
-        var iw = new gm.InfoWindow();
-        var oms = new OverlappingMarkerSpiderfier(map,
-          { markersWontMove: true, markersWontHide: true });
-
-        var iconWithColor = function (color) {
-            var icon;
-            if (color == 'usual') {
-                icon = '/img/especialista-pointer.png';
-            }
-            else if (color == 'spider') {
-                icon = '/img/especialista-pointer-spider.png';
-            }
+    //     var iconWithColor = function (color) {
+    //         var icon;
+    //         if (color == 'usual') {
+    //             icon = '/img/especialista-pointer.png';
+    //         }
+    //         else if (color == 'spider') {
+    //             icon = '/img/especialista-pointer-spider.png';
+    //         }
             
-            return icon;
-        }
+    //         return icon;
+    //     };
 
-        oms.addListener('click', function (marker) {
-            iw.setContent(marker.desc);
-            iw.open(map, marker);
-        });
+    //     oms.addListener('click', function (marker) {
+    //         iw.setContent(marker.desc);
+    //         iw.open(map, marker);
+    //     });
 
-        oms.addListener('spiderfy', function (markers) {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setIcon(iconWithColor('spider'));
-                markers[i].setShadow(null);
-            }
-            iw.close();
-        });
+    //     oms.addListener('spiderfy', function (markers) {
+    //         for (var i = 0; i < markers.length; i++) {
+    //             markers[i].setIcon(iconWithColor('spider'));
+    //             markers[i].setShadow(null);
+    //         }
+    //         iw.close();
+    //     });
 
-        oms.addListener('unspiderfy', function (markers) {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setIcon(iconWithColor('usual'));
-                markers[i].setShadow(null);
-            }
-        });
+    //     oms.addListener('unspiderfy', function (markers) {
+    //         for (var i = 0; i < markers.length; i++) {
+    //             markers[i].setIcon(iconWithColor('usual'));
+    //             markers[i].setShadow(null);
+    //         }
+    //     });
 
-        var bounds = new gm.LatLngBounds();
+    //     var bounds = new gm.LatLngBounds();
 
-        $.ajax({
-            type: "GET",
-            url: "/Pontos.xml",
-            dataType: "xml",
-            success: xmlParser
-        });
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "/Pontos-de-Interesse.kml",
+    //         dataType: "xml",
+    //         success: xmlParser
+    //     });
 
-        function xmlParser(xml) {
-            var latlngbounds = new google.maps.LatLngBounds();
+    //     function xmlParser(xml) {
+    //         var latlngbounds = new google.maps.LatLngBounds();
 
-            $(xml).find("Especialista").each(function () {
-                var idPonto = $(this).find("Id").text();
-                var nome = $(this).find("Nome").text();
-                var end = $(this).find("Endereco").text();
-                var tel = $(this).find("Telefone").text();
-                var lat = $(this).find("Latitude").text();
-                var long = $(this).find("Longitude").text();
-                var desc = $(this).find("Descricao").text();
-                var dest = $(this).find("Destaque").text();
-                var logo = $(this).find("Logo").text();
+    //         $(xml).find("Placemark").each(function () {
+    //             var name = $(this).find("name").text();
+    //             var coordinates = $(this).find("Point").find("coordinates").text();
+    //             var lat = coordinates.split(',')[1];
+    //             var long = coordinates.split(',')[0];
 
-                var loc = new gm.LatLng(lat, long);
-                bounds.extend(loc);
-                var marker = new gm.Marker({
-                    position: loc,
-                    title: nome,
-                    map: map,
-                    icon: iconWithColor('usual')
-                });
+    //             var loc = new gm.LatLng(lat, long);
+    //             bounds.extend(loc);
+    //             var marker = new gm.Marker({
+    //                 position: loc,
+    //                 title: name,
+    //                 map: map,
+    //                 icon: iconWithColor('usual')
+    //             });
 
-                marker.desc = "<div style='min-width: 480px; min-height: 50px'><img src='/img/" + logo + "' alt='" + nome + "' /></div><p><b>" + nome + "</b></p><p>" + desc + "</p>" + end + "<br />" + tel
-                oms.addMarker(marker);
-                markersArray.push(marker);
-            });
-
-        }
-    }
-
-    map.fitBounds(bounds);
-}
+    //             marker.desc = '<h3>' + name + '</h3>';
+    //             oms.addMarker(marker);
+    //             markersArray.push(marker);
+    //         });
+    //     }
+        
+    //     map.fitBounds(bounds);
+    // }
+};
