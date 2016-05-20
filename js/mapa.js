98a2f2ca104851d6975d3d5742060d9ec140e89b
Felipe blini <Felipe.blini@gmail.com>
2016-05-20 15:20:16 -0300

    added jquery-ui autocomplete and have done function to retrieve interested points

--- js/mapa.js
+++ js/mapa.js
@@ -1,58 +1,139 @@
-window.onload = function () {
-    var markersArray = [];
+$(document).ready(function () {
+    
     var gm = google.maps;
+    var geocoder = new gm.Geocoder();
+    var marker;
 
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            //'Geolocation is not supported by this browser.';
+            showMap(-15.834295, -48.023389);
+        }
+    }
+    
+    function showPosition(position) {
+        showMap(position.coords.latitude, position.coords.longitude);
+    }
+    
+    getLocation();
+    
+    function showMap(userLat, userLong) {
+        var latlng = new gm.LatLng(userLat, userLong);
+        var map = new gm.Map(document.getElementById('mapa'), {
+            mapTypeId: gm.MapTypeId.ROADMAP,
+            center: latlng, zoom: 5
+        });
+        
+        marker = new gm.Marker({
+            map: map,
+            draggable: true
+        });
+    
+        marker.setPosition(latlng);
+    }
 
-    var latlng = new gm.LatLng(-15.834295, -48.023389);
-    var map = new gm.Map(document.getElementById('map_canvas'), {
-        mapTypeId: gm.MapTypeId.ROADMAP,
-        center: latlng, zoom: 15
-
+    // var kmlUrl2 = 'http://felipeblini-001-site32.btempurl.com/PontosInteresse.kml?v=2';
+    // var kmlOptions = {
+    //     suppressInfoWindows: false,
+    //     preserveViewport: false,
+    //     map: map
+    // };
+    
+    // var kmlLayer = new gm.KmlLayer(kmlUrl2, kmlOptions);
+    
+    function addressAutocompleate(direction) {
+        return {
+            source: function (request, response) {
+            
+                geocoder.geocode({ 'address': request.term + ', Brasil', 'region': 'BR' }, function (results, status) {
+                    response($.map(results, function (item) {
+                        return {
+                            label: item.formatted_address,
+                            value: item.formatted_address,
+                            latitude: item.geometry.location.lat(),
+                            longitude: item.geometry.location.lng()
+                        };
+                    }));
+                });
+            },
+            select: function (event, ui) {
+                $('#txtLatitude' + direction).val(ui.item.latitude);
+                $('#txtLongitude' + direction).val(ui.item.longitude);
+                var location = new gm.LatLng(ui.item.latitude, ui.item.longitude);
+                //marker.setPosition(location);
+                //map.setCenter(location);
+                //map.setZoom(16);
+            }
+        };
+    }
+    
+    $('#txtEnderecoOrigem').autocomplete(addressAutocompleate('Origem'));
+    
+    $('#txtEnderecoDestino').autocomplete(addressAutocompleate('Destino'));
+    
+    $('#goSubmit').on('click', function(e) {
+        //e.preventDefault();
+        
+        var origemLat = $('#txtLatitudeOrigem').val();
+        var origemLong = $('#txtLongitudeOrigem').val();
+        
+        var destinoLat = $('#txtLatitudeDestino').val();
+        var destinoLong = $('#txtLongitudeDestino').val(); 
+        
+        goButton(origemLat, origemLong, destinoLat, destinoLong);
+        
+        return false;
     });
+    
+    function goButton(oLat, oLong, dLat, dLong) {
+        var origemLatLong = gm.LatLng(oLat, oLong);
+        var destinoLatLong = gm.LatLng(dLat, dLong);
+        var getIntermediatePoints = new IntermediatePoints();
+        getIntermediatePoints.getItens(foo);
+        
+        function foo(result) {
+            console.log(result);
+            console.log(this);
+        }
+    }
 
-    var kmlUrl2 = 'http://felipeblini-001-site32.btempurl.com/PontosInteresse.kml?v=2';
-    var kmlOptions = {
-        suppressInfoWindows: false,
-        preserveViewport: false,
-        map: map
+    function IntermediatePoints() { }
+    
+    IntermediatePoints.prototype.getItens = function(callback) {
+        var intermetiatePoints = [];
+        
+        $.ajax({
+            type: 'GET',
+            url: '/PontosInteresse.kml',
+            dataType: 'xml',
+            success: xmlParser
+        });
+
+        function xmlParser(xml) {
+            $(xml).find('Placemark').each(function () {
+                var name = $(this).find('name').text();
+                var coordinates = $(this).find('Point').find('coordinates').text();
+                var lat = coordinates.split(',')[1];
+                var long = coordinates.split(',')[0];
+
+                var loc = new gm.LatLng(lat, long);
+
+                var point = {
+                    name: name,
+                    coordinates: coordinates,
+                    loc: loc
+                };
+                
+                intermetiatePoints.push(point);
+
+            });
+            
+            callback.call(this, intermetiatePoints);
+        }
     };
-    
-    
-    var kmlLayer = new google.maps.KmlLayer(kmlUrl2, kmlOptions);
-
-    //     var bounds = new gm.LatLngBounds();
-
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "/Pontos-de-Interesse.kml",
-    //         dataType: "xml",
-    //         success: xmlParser
-    //     });
-
-    //     function xmlParser(xml) {
-    //         var latlngbounds = new google.maps.LatLngBounds();
-
-    //         $(xml).find("Placemark").each(function () {
-    //             var name = $(this).find("name").text();
-    //             var coordinates = $(this).find("Point").find("coordinates").text();
-    //             var lat = coordinates.split(',')[1];
-    //             var long = coordinates.split(',')[0];
-
-    //             var loc = new gm.LatLng(lat, long);
-    //             bounds.extend(loc);
-    //             var marker = new gm.Marker({
-    //                 position: loc,
-    //                 title: name,
-    //                 map: map,
-    //                 icon: iconWithColor('usual')
-    //             });
-
-    //             marker.desc = '<h3>' + name + '</h3>';
-    //             oms.addMarker(marker);
-    //             markersArray.push(marker);
-    //         });
-    //     }
         
     //     map.fitBounds(bounds);
     // }
-};
+});
