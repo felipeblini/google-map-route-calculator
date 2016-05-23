2349a968d7c60a037ed74e3010dcdefd6020010f
Felipe blini <Felipe.blini@gmail.com>
2016-05-23 18:24:30 -0300

    initial commit

---
+++ js/intermediatePoints.js
@@ -0,0 +1,39 @@
+var IntermediatePoints = (function() {
+    'use strict';
+    
+    function IntermediatePoints() { }
+        
+    IntermediatePoints.prototype.getItems = function(callback) {
+        var intermetiatePoints = [];
+        
+        $.ajax({
+            type: 'GET',
+            url: '/PontosInteresse.kml',
+            dataType: 'xml',
+            success: xmlParser
+        });
+        
+        function xmlParser(xml) {
+            $(xml).find('Placemark').each(function () {
+                var name = $(this).find('name').text();
+                var coordinates = $(this).find('Point').find('coordinates').text();
+                var lat = coordinates.split(',')[1];
+                var long = coordinates.split(',')[0];
+
+                var point = {
+                    name: name,
+                    coordinates: coordinates,
+                    lat: lat,
+                    long: long
+                };
+                
+                intermetiatePoints.push(point);
+            });
+            
+            callback.call(this, {intermetiatePoints: intermetiatePoints});
+        }
+    };
+    
+    return IntermediatePoints;
+
+})();
